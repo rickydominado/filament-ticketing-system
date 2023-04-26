baa99b454cd8473f24edbd437b71e13f966fed5b
Ricky Dominado <45351051+rickydominado@users.noreply.github.com>
2023-04-26 15:52:30 +0800

    Created Inquiry factory and seeder

--- database/factories/InquiryFactory.php
+++ database/factories/InquiryFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Inquiry;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,25 @@ class InquiryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'title' => fake()->sentence(3),
+            'content' => fake()->paragraph(),
+            'category_id' => rand(1, 3),
+            'status' => \App\Enums\Inquiry\Status::Open->value,
+            'severity' => \App\Enums\Inquiry\Severity::Low->value,
         ];
+    }
+
+    /**
+     * Configure the model factory.
+     */
+    public function configure(): static
+    {
+        return $this->afterCreating(function (Inquiry $inquiry) {
+            foreach (range(1, rand(1, 3)) as $i) {
+                $inquiry->addMediaFromUrl("https://picsum.photos/1200/800")->toMediaCollection('images');
+            }
+        });
     }
 }
